dac799be96b1e246242d77772a572da287e37757
Muraleekrishna G <muraleekrishnagc@gmail.com>
2017-12-21 11:51:26 +0530

    Fix: Add child bug

--- app/models/person.rb
+++ app/models/person.rb
@@ -20,9 +20,9 @@ class Person < ApplicationRecord
     @child.primary = true
     child_hash = @child.as_json.except('ancestry')
     if primary?
-      children.create!(child_hash)
+      self.children.create!(child_hash)
     else
-      spouse.create!(child_hash)
+      self.spouse.create!(child_hash)
     end
   end
 
